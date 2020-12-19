378a04fdd60d42207411f845661e646eb93ad916
saggers <sagarparmar0233@gmail.com>
2020-12-18 19:06:46 -0800

    completed simple list and spread operator

--- book-list/src/index.js
+++ book-list/src/index.js
@@ -5,31 +5,65 @@ import ReactDom from "react-dom";
 import "./index.css";
 //set up
 
-const firstBook = {
-  author: "Mark Manson",
-  title: "The Subtle Art of Not Giving a F*ck",
-  img: "https://m.media-amazon.com/images/I/71t4GuxLCuL._AC_UL320_.jpg",
-};
+const books = [
+  {
+    id: 1,
+    author: "Mark Manson",
+    title: "The Subtle Art of Not Giving a F*ck",
+    img: "https://m.media-amazon.com/images/I/71t4GuxLCuL._AC_UL320_.jpg",
+  },
 
-const secondBook = {
-  author: "Amelia Hepworth and Tim Warnes",
-  title: "I Love You to the Moon and Back",
-  img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg",
-};
+  {
+    id: 2,
+    author: "Amelia Hepworth and Tim Warnes",
+    title: "I Love You to the Moon and Back",
+    img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg",
+  },
+  {
+    id: 3,
+    author: "Toni Morrison",
+    title: "Beloved",
+    img: "https://m.media-amazon.com/images/I/613vQdXPDwL._AC_UL320_.jpg",
+  },
+];
+
+// const names = ["sagar", "simmu", "parmar"];
+// const newNames = names.map((name) => {
+//   return <h1>{name}</h1>;
+// });
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
+      {books.map((book) => {
+        {
+          /* const { author, title, img } = book; */
+        }
+        //here we are passing book object as a props and than from there we can destructure the book
+        //object and than print the value
+        {
+          /* return <Book key={book.id} book={book} />; */
+        }
+        return <Book key={book.id} {...book} />;
+      })}
+
+      {/* Example of children props */}
+      {/* <Book
         author={firstBook.author}
         title={firstBook.title}
         img={firstBook.img}
-      />
-      <Book
+      > */}
+      {/* Whatever we would put in here would be stored in props and in the 'children' attribute */}
+      {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        quasi minima quisquam, rerum at voluptates eum fuga officiis quod
+        maiores? */}
+      {/* </Book> */}
+
+      {/* <Book
         author={secondBook.author}
         title={secondBook.title}
         img={secondBook.img}
-      />
+      /> */}
     </section>
   );
 }
@@ -38,12 +72,21 @@ const Book = (props) => {
   // console.log(props);
 
   //destructuring of the props object and than extracting the properties img, title, author
-  const { img, title, author } = props;
+  //the name of the "children props" must always be a "children"
+  // const { img, title, author, children } = props.book;
+
+  //if we use spread '...' operator before the object than we can just use simple props
+  //destructuring
+
+  const { author, title, img } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author} </h4>
+
+      {/* Here we can access children from props that is in between the tags of the component  */}
+      {/* <p>{children}</p> */}
 
       {/* {console.log(props)} */}
       {/* This won't work as whatever that is passed inside '{}' should return something. In this case
